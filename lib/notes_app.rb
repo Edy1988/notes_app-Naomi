11357a230344f4da357f2ddebdd6bc23413c55c2
NaomiBid <naomi0bid@gmail.com>
2018-10-16 13:56:24 +0100

    created todolist app

--- lib/notes_app.rb
+++ lib/notes_app.rb
@@ -1,6 +1,29 @@
-class Notes
+class Todo
 
-  def add_note
-    "Hello"
+  def initialize(string)
+    @string = string
   end
+
+  def text
+    @string
+  end
+
+end
+
+class TodoList
+
+  def initialize
+    @list = []
+  end
+
+  def add(todo)
+    @list.push(todo.text)
+  end
+
+  def print
+    @list.each do
+      |i| puts "* " + i
+    end
+  end
+
 end
